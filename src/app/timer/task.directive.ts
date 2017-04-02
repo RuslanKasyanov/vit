f44de4574913a4e06f97ad9291d7f5ce1e7e6ff4
RuslanKasyanov <rkasyanov@evelopars.com>
2017-04-02 22:21:10 +0300

    pipe

---
+++ src/app/timer/task.directive.ts
@@ -0,0 +1,24 @@
+import {Directive, Input, HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[task]'
+})
+export class TaskDirective {
+
+  private defaultTooltipText: string;
+  @Input() task: Task;
+  @Input() taskTooltip: any;
+  @HostListener('mouseover') onMouseOver() {
+    if (!this.defaultTooltipText && this.taskTooltip) {
+      this.defaultTooltipText = this.taskTooltip.innerText;
+    }
+    this.taskTooltip.innerText = this.task.name;
+  }
+  @HostListener('mouseout') onMouseOut () {
+    if(this.taskTooltip) {
+      this.taskTooltip.innerText = this.defaultTooltipText;
+    }
+  }
+  constructor() { }
+
+}
