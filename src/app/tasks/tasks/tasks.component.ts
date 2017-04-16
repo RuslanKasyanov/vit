8fa4d12e21ce6f1fa0ff07fb0c7d6501395d12ba
RuslanKasyanov <rkasyanov@evelopars.com>
2017-04-17 00:35:16 +0300

    tree comp

--- src/app/tasks/tasks/tasks.component.ts
+++ src/app/tasks/tasks/tasks.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import TaskIconsComponent from '../task-icons/task-icons.component';
+import TaskTooltipDirective from '../task-tooltip.directive';
+import {
+  TaskService,
+  SettingsService,
+  Task,
+  SHARED_PIPES
+  } from '../../shared/shared';
 
 @Component({
   selector: 'app-tasks',
+  entryComponents: [TaskTooltipDirective, TaskTooltipDirective],
+  pipes: [SHARED_PIPES],
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export default class TasksComponent implements OnInit {
+ today: Date;
+ tasks: Task[];
+ queuedPomodoros: number;
+ queueHeaderMapping: any;
+ timerMinutes: number;
+  constructor(
+    private taskService: TaskService,
+    private settingsService: SettingsService) {
+    this.tasks = this.taskService.taskStore;
+    this.today = new Date();
+    this.queueHeaderMapping = settingsService.pluralsMap.tasks;
+    this.timerMinutes = settingsService.timerMinutes;
+  }
+  ngOnInit():void {
+    this.updateQueuedPomodoros();
+  }
+  toggleTask(task: Task): void {
+    task.queued = !task.queued;
+    this.updateQueuedPomodoros();
+  }
+  private updateQueuedPomodoros(): void {
+    this.queuedPomodoros = this.tasks.filter((task: Task) => task.queued)
+      .reduce((pomodoros: number, queuedTask: Task) => {
+       return pomodoros + queuedTask.pomodorosRequired;
+      }, 0);
   }
 
 }
